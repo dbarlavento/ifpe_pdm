f49fb6bbb1a87b1c7969a7c308d6312690964312
Daniel Barlavento <dbg@a.recife.ifpe.edu.br>
2016-10-24 12:25:35 -0300

    Refatoracaoa da tela de login.

--- Pratica_02/app/src/main/java/barlapp/pratica_02/MainActivity.java
+++ Pratica_02/app/src/main/java/barlapp/pratica_02/MainActivity.java
@@ -1,5 +1,6 @@
 package barlapp.pratica_02;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,31 +18,12 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
-		Toolbar mainToolBar = (Toolbar) findViewById(R.id.main_toolbar);
-		setSupportActionBar(mainToolBar);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main_menu, menu);
-		return true;
-	}
+	/* Realiza login no sistema */
+	public void entrar( View view ) {
+		Intent intent = new Intent( this, MapActivity.class );
+		startActivity( intent );
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.acao_configurar:
-				Toast.makeText( this, "Configura o sistema", Toast.LENGTH_SHORT ).show();
-				return true;
-
-			case R.id.acao_pesquisar:
-				Toast.makeText( this, "Pesquisar Frutas", Toast.LENGTH_SHORT ).show();
-				return true;
-
-			default:
-				return super.onOptionsItemSelected(item);
-
-		}
 	}
 }
