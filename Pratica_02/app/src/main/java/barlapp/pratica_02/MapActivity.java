fccbdcd22ccf31cd3d2f00d15d445625f1d760d2
Daniel Barlavento <dbg@a.recife.ifpe.edu.br>
2016-10-24 14:08:58 -0300

    Tela do mapa faltando so o mapa, pega localizacao do usuario.

--- Pratica_02/app/src/main/java/barlapp/pratica_02/MapActivity.java
+++ Pratica_02/app/src/main/java/barlapp/pratica_02/MapActivity.java
@@ -1,14 +1,69 @@
 package barlapp.pratica_02;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MapActivity extends AppCompatActivity {
+
+	LocationManager locationManager;
+
+	private void setInitLocation() {
+		if ( ActivityCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission( this, Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		locationManager.requestSingleUpdate( LocationManager.GPS_PROVIDER, new LocationListener() {
+
+			@Override
+			public void onLocationChanged( Location location ) {
+				TextView latitude = ( TextView ) findViewById( R.id.latitude );
+				TextView longitude = ( TextView ) findViewById( R.id.longitude );
+
+				if ( location != null ) {
+					latitude.setText("Latitude: " + location.getLatitude());
+					longitude.setText("Longitude: " + location.getLongitude());
+				} else {
+					latitude.setText( "Latitude: Não localizado" );
+					longitude.setText( "Longitude: Não localizado" );
+				}
+
+			}
+
+			@Override
+			public void onStatusChanged( String provider, int status, Bundle extras ) {
+
+			}
+
+			@Override
+			public void onProviderEnabled( String provider ) {
+
+			}
+
+			@Override
+			public void onProviderDisabled( String provider ) {
+
+			}
+		}, null );
+	}
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
@@ -16,6 +71,8 @@ public class MapActivity extends AppCompatActivity {
 		setContentView( R.layout.activity_map );
 		Toolbar mainToolBar = (Toolbar) findViewById(R.id.main_toolbar);
 		setSupportActionBar(mainToolBar);
+		locationManager = ( LocationManager ) getSystemService( Context.LOCATION_SERVICE );
+		setInitLocation();
 	}
 
 	@Override
@@ -49,4 +106,5 @@ public class MapActivity extends AppCompatActivity {
 
 		}
 	}
+
 }
---
+++ Pratica_02/app/src/main/java/barlapp/pratica_02/Posicao.java
@@ -0,0 +1,31 @@
+package barlapp.pratica_02;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by user on 24/10/16.
+ */
+
+public class Posicao implements LocationListener {
+	@Override
+	public void onLocationChanged( Location location ) {
+
+	}
+
+	@Override
+	public void onStatusChanged( String provider, int status, Bundle extras ) {
+
+	}
+
+	@Override
+	public void onProviderEnabled( String provider ) {
+
+	}
+
+	@Override
+	public void onProviderDisabled( String provider ) {
+
+	}
+}
