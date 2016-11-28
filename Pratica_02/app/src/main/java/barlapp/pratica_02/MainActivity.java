d45e6a240a5a1fc42c7ef8cf028d779534f88fa2
dbarlavento <dbg@a.recife.ifpe.edu.br>
2016-11-28 17:54:25 -0300

    Modificações adicionado botão de nova árvore na tela do mapa

--- Pratica_02/app/src/main/java/barlapp/pratica_02/MainActivity.java
+++ Pratica_02/app/src/main/java/barlapp/pratica_02/MainActivity.java
@@ -1,16 +1,9 @@
 package barlapp.pratica_02;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
