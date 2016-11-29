7fc9e1f8a8da5c2e8286aac56ca335e210f491b2
dbarlavento <dbg@a.recife.ifpe.edu.br>
2016-11-29 17:25:32 -0300

    Adicionada tela de recuperação de senha

--- FrutaGratis/app/src/main/java/barlapcb/frutagratis/InitActivity.java
+++ FrutaGratis/app/src/main/java/barlapcb/frutagratis/InitActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class InitActivity extends AppCompatActivity {
+
+    private String email;
+    private String senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +18,28 @@ public class InitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_init);
     }
 
+    private boolean validarLogin(String e, String s) {
+        return true;
+    }
+
     /* Realiza login no sistema */
     public void entrar( View view ) {
-
+        if( validarLogin(email, senha) ) {
+            Intent intent = new Intent( this, MapActivity.class );
+            startActivity( intent );
+        } else {
+            Toast.makeText( this, "E-mail ou senha incorretos", Toast.LENGTH_LONG ).show();
+        }
     }
 
     /* Registra um novo usuário */
     public void registrarNovoUsuario(View view) {
         Intent intent = new Intent( this, RegistrarActivity.class );
+        startActivity( intent );
+    }
+
+    public void recuperarSenha(View view) {
+        Intent intent = new Intent( this, RecupSenhaActivity.class );
         startActivity( intent );
     }
 }
--- FrutaGratis/app/src/main/java/barlapcb/frutagratis/RegistrarActivity.java
+++ FrutaGratis/app/src/main/java/barlapcb/frutagratis/RegistrarActivity.java
@@ -1,6 +1,7 @@
 package barlapcb.frutagratis;
 
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,10 +15,11 @@ public class RegistrarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registrar);
         Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar_registrar);
         setSupportActionBar(toolBar);
+        ActionBar actBar = getSupportActionBar();
+        actBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+    private void persistirUsuario() {
+
     }
 }
