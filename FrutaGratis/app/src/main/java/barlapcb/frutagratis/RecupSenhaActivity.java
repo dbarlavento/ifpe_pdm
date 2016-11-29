bc153de182a9b119b67fd401852f74078bf9141c
dbarlavento <dbg@a.recife.ifpe.edu.br>
2016-11-29 17:28:43 -0300

    Implementado botão de voltar da tela de recuperação de senha

--- FrutaGratis/app/src/main/java/barlapcb/frutagratis/RecupSenhaActivity.java
+++ FrutaGratis/app/src/main/java/barlapcb/frutagratis/RecupSenhaActivity.java
@@ -1,7 +1,9 @@
 package barlapcb.frutagratis;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RecupSenhaActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class RecupSenhaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recup_senha);
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar_registrar);
+        setSupportActionBar(toolBar);
+        ActionBar actBar = getSupportActionBar();
+        actBar.setDisplayHomeAsUpEnabled(true);
     }
 }
